9aaf2ee799c38c19e13efdfe280e5111ea82f745
Michael Bayne <mdb@samskivert.com>
2008-01-04 03:47:19 +0000

    Use the right problem number.

---
+++ Euler28.scala
@@ -0,0 +1,14 @@
+object Euler28 extends Application {
+  def spiral (size :Int) :Int = {
+    if (size == 1) {
+      return 1;
+    } else {
+      val smaller = size-2;
+      val start = smaller*smaller;
+      val step = smaller+1;
+      return spiral(smaller) + ((start + 1*step) + (start + 2*step) +
+                                (start + 3*step) + (start + 4*step));
+    }
+  }
+  println(spiral(1001));
+}
