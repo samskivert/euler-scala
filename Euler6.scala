0cea42616512135cb0b326197a8b5918dafa8492
Michael Bayne <mdb@samskivert.com>
2008-01-09 09:04:55 +0000

    Formatting for blog postage.

--- Euler6.scala
+++ Euler6.scala
@@ -1,7 +1,5 @@
 object Euler6 extends Application {
   val first100sum = List.range(1,101).foldLeft(0)(_+_);
   val first100sumsq = List.range(1,101).foldLeft(0)((b, a) => (b + a*a));
-  println("Sum of squares: " + first100sumsq);
-  println("Square of sums: " + first100sum * first100sum);
-  println("Difference: " + (first100sum * first100sum - first100sumsq));
+  println(first100sum * first100sum - first100sumsq);
 }
