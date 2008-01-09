b9ff560fc8bef4d8f72676ddfea82ef3fca6952d
Michael Bayne <mdb@samskivert.com>
2008-01-09 20:41:37 +0000

    Brevity++.

--- Euler1.scala
+++ Euler1.scala
@@ -1,6 +1,3 @@
 object Euler1 extends Application {
-  def div3or5 (from: Int, to: Int): List[Int] = {
-    for (i <- List.range(from, to) if i % 3 == 0 || i % 5 == 0) yield i
-  }
-  println(div3or5(0, 1000).foldLeft(0)(_+_))
+  println(List.range(0, 1000).filter((a) => (a % 3 == 0 || a % 5 == 0)).foldLeft(0)(_+_));
 }
