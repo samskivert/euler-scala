f91cb5aa4a89a72abedab0e6285ec7c09fbbd6da
Michael Bayne <mdb@samskivert.com>
2008-01-19 21:11:07 +0000

    Performance fiddling.

---
+++ Euler23.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+
+public class Euler23
+{
+    public static final int MAX = 28123;
+
+    public static void main (String[] args)
+    {
+        long start = System.currentTimeMillis();
+        int[] abundant = new int[MAX];
+        int acount = 0;
+        for (int ii = 1; ii <= MAX; ii++) {
+            if (ii < sumdiv(ii)) {
+                abundant[acount++] = ii;
+            }
+        }
+
+        HashSet<Integer> integers = new HashSet<Integer>();
+        for (int ii = 0; ii < MAX; ii++) {
+            integers.add(ii+1);
+        }
+
+        for (int aa = 0; aa < acount; aa++) {
+            for (int bb = aa; bb < acount; bb++) {
+                integers.remove(abundant[aa] + abundant[bb]);
+            }
+        }
+
+        int total = 0;
+        for (Integer value : integers) {
+            total += value;
+        }
+        System.out.println(total);
+        System.out.println("[total " + (System.currentTimeMillis() - start) + "ms]");
+    }
+
+    protected static int sumdiv (int value)
+    {
+        int sum = 1;
+        for (int ii = 2, ll = (int)Math.ceil(Math.sqrt(value)); ii < ll; ii++) {
+            if (value % ii == 0) {
+                sum += ii;
+                sum += (value / ii);
+            }
+        }
+        double root = Math.sqrt(value);
+        int iroot = (int)root;
+        if (root > 1 && root == iroot) {
+            sum += iroot;
+        }
+        return sum;
+    }
+}
--- Euler23.scala
+++ Euler23.scala
@@ -1,28 +1,20 @@
 import scala.collection.mutable.Set;
 
 object Euler23 extends Application {
-  def sum (values :List[Int]) = {
-    values.foldLeft(0)(_+_)
-  }
-
-  def divisors (x :Int) :List[Int] = {
-    1 :: List.flatten(for { 
-      divis <- List.range(2, Math.sqrt(x)+1)
-      if x % divis == 0
-    } yield List(divis, x / divis).removeDuplicates);
-  }
+  def sumdiv (x :Int) :Int = (1 :: List.flatten(for { 
+    divis <- List.range(2, Math.sqrt(x)+1)
+    if x % divis == 0
+  } yield List(divis, x / divis).removeDuplicates)).foldLeft(0)(_+_);
 
   val max = 28123;
-  var abundant = (for {
-    a <- List.range(1, max+1)
-    if (a < sum(divisors(a)))
-  } yield a).toArray;
+  val abundant = List.range(1, max+1).filter(a => (a < sumdiv(a))).toArray;
 
-  var integers = Set() ++ List.range(1, max+1);
-  for (a <- List.range(0, abundant.length)) {
-    for (b <- List.range(a, abundant.length)) {
-      integers -= (abundant(a) + abundant(b));
-    }
+  def filter (abund :Array[Int], ints :Set[Int], a :Int, b :Int) :Set[Int] = {
+    ints -= (abund(a) + abund(b));
+    if (b == abund.length-1)
+      if (a == abund.length-1) return ints;
+      else return filter(abund, ints, a+1, 0);
+    else return filter(abund, ints, a, b+1);
   }
-  println(sum(integers.toList));
+  println(filter(abundant, Set() ++ List.range(1, max+1), 0, 0).foldLeft(0)(_+_));
 }
