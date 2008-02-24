f59a7aa1b56a0a59d0c57daf9750ea41b83dc7ee
Michael Bayne <mdb@samskivert.com>
2008-02-24 03:27:16 +0000

    Clarifying.

--- Euler28.scala
+++ Euler28.scala
@@ -2,7 +2,7 @@ object Euler28 extends Application {
   def spiral (size :Int) :Int = {
     if (size == 1) return 1;
     val smaller = size-2;
-    spiral(smaller) + 4*(smaller*smaller) + 10*(smaller+1);
+    spiral(smaller) + 4*(smaller*smaller) + (1+2+3+4)*(smaller+1);
   }
   println(spiral(1001));
 }
