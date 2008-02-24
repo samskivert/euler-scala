fd8987e67e69f4e243fa8714afab9296bc2dd03b
Michael Bayne <mdb@samskivert.com>
2008-02-24 03:46:23 +0000

    Blog prep.

--- Euler30.scala
+++ Euler30.scala
@@ -1,11 +1,5 @@
 object Euler30 extends Application {
-  def digits (value :Int) :List[Int] = {
-    if (value == 0) return Nil;
-    else return (value % 10) :: digits(value/10);
-  }
-
-  println((for {
-    value <- List.range(2, 200000)
-    if (digits(value).map((a) => Math.pow(a, 5).intValue()).foldRight(0)(_+_) == value)
-  } yield value).foldRight(0)(_+_));
+  def digits (n :Int) = n.toString.toList.map(c => c - '0');
+  def issum5 (n :Int) = digits(n).map(a => Math.pow(a, 5).intValue()).foldRight(0)(_+_) == n;
+  println(List.range(2, 200000).filter(issum5).foldRight(0)(_+_));
 }
