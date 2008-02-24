19ac49411aeeac45a0aa87a223378e1aa038d745
Michael Bayne <mdb@samskivert.com>
2008-02-24 04:06:51 +0000

    Tidying up for bloggage.

--- Euler31.scala
+++ Euler31.scala
@@ -1,13 +1,9 @@
 object Euler31 extends Application {
   def perms (remain :Int, coins :List[Int]) :Int = {
-    if (remain == 0) {
-      return 1;
-    } else if (coins == Nil) {
-      return 0;
-    } else {
-      def mapf (q :Int) = perms(remain - q*coins.head, coins.drop(1));
-      return List.range(0, remain/coins.head+1).map(mapf).foldRight(0)(_+_);
-    }
+    if (remain == 0) return 1;
+    else if (coins == Nil) return 0;
+    else List.range(0, remain/coins.head+1).map(
+      q => perms(remain - q*coins.head, coins.tail)).foldRight(0)(_+_);
   }
   println(perms(200, List(200, 100, 50, 20, 10, 5, 2, 1)));
 }
--- Euler32.scala
+++ Euler32.scala
@@ -1,14 +1,8 @@
 object Euler32 extends Application {
-  def digits (value :Int) :List[Int] = {
-    if (value == 0) return Nil;
-    else return (value % 10) :: digits(value / 10);
-  }
-  def ispan (div :Int, mult :Int, value :Int) :Boolean = {
-    return (digits(div) ::: digits(value) ::: digits(mult)).sort(_<_).equals(List.range(1,10));
-  }
-  def haspanfact (value :Int) :Boolean = {
-    return List.range(2, 100).find(
-      (div) => (value % div == 0 && ispan(div, value/div, value))) != None;
-  }
+  def digits (n :Int) = n.toString.toList.map(c => c - '0');
+  def ispan (a :Int, b :Int, n :Int) :Boolean =
+    (digits(a) ::: digits(n) ::: digits(b)).sort(_<_).equals(List.range(1,10));
+  def haspanfact (n :Int) :Boolean =
+    List.range(2, 100).find((a) => (n % a == 0 && ispan(a, n/a, n))) != None;
   println(List.range(1000, 10000).filter(haspanfact).foldRight(0)(_+_));
 }
