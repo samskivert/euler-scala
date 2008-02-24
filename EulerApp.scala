1c38f96f8a7ec144803858e0529af64c6cfff593
Michael Bayne <mdb@samskivert.com>
2008-02-24 01:54:41 +0000

    Code--.

--- EulerApp.scala
+++ EulerApp.scala
@@ -10,15 +10,10 @@ class EulerApp extends Application {
     val primes = List.range(0,range).toArray;
     primes(1) = 0;
     var idx = 2;
-    while (idx >= 0) {
-      var prime = primes(idx);
-      var midx = idx + prime;
-      while (midx < primes.length) {
-        primes(midx) = 0;
-        midx += prime;
-      }
-      val nidx = primes.slice(idx+1, primes.length).findIndexOf((a) => (a != 0));
-      if (nidx == -1) idx = -1 else idx = idx + 1 + nidx;
+    while (idx < range) {
+      for (midx <- List.range(idx+idx, primes.length, idx)) primes(midx) = 0;
+      do idx = idx+1;
+      while (idx < range && primes(idx) == 0);
     }
     return primes;
   }
