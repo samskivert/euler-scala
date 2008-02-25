4a206844dce5e091dbe4661576fa84efe3a7373c
Michael Bayne <mdb@samskivert.com>
2008-02-25 18:51:38 +0000

    Link EulerApp to its source.

--- blog.pl
+++ blog.pl
@@ -4,5 +4,11 @@ my $number = shift or die "Usage: $0 N\n";
 my $url = "http://projecteuler.net/index.php";
 printf("<a href=\"$url?section=problems&id=%d\">Problem %03d</a>:\n", $number, $number);
 print("<pre>\n");
-print(`sed 's:<:\\&lt;:g' Euler$number.scala`);
+open(CODE, "Euler$number.scala") or die "Can't read Euler$number.scala: $!\n";
+while (<CODE>) {
+    $_ =~ s#<#\&lt;#g;
+    $_ =~ s#EulerApp#<a href="/code/euler/EulerApp.scala">EulerApp</a>#g;
+    print $_;
+}
+# print(`sed '' Euler$number.scala`);
 print("</pre>\n");
