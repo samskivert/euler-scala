9d70ccfc4463091f0a46e48a4c2a80b2aaf834b8
Michael Bayne <mdb@samskivert.com>
2008-02-26 07:04:29 +0000

    Pre-blog tidying.

--- Euler35.scala
+++ Euler35.scala
@@ -2,11 +2,11 @@ object Euler35 extends EulerApp {
   val primes = genprimes(1000000);
   def digits (value :Int) :Int = if (value == 0) 0 else 1 + digits(value/10);
   def rotate (value :Int, turns :Int) :Int = {
-    if (turns == 0) value;
-    else rotate(value%10 * Math.pow(10, digits(value)-1).intValue() + value/10, turns-1);
+    val mod = Math.pow(10, digits(value)-turns).intValue()
+    (value % mod) * Math.pow(10, turns).intValue() + (value / mod)
   }
   def circprime (n :Int) :Boolean = {
-    List.range(0, digits(n)).foldRight(true)((turns, b) => (b && (primes(rotate(n, turns)) != 0)))
+    List.range(0, digits(n)).foldRight(true)((t, b) => (b && (primes(rotate(n, t)) != 0)))
   }
-  println(List.range(2,1000000).filter((n) => (primes(n) != 0)).filter(circprime).length);
+  println(primes.filter(0.!=).filter(circprime).length);
 }
