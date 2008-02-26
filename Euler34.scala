aaeff946006d60fc291aee32bf8ab7aec89c0a40
Michael Bayne <mdb@samskivert.com>
2008-02-26 06:40:18 +0000

    Refinement.

--- Euler34.scala
+++ Euler34.scala
@@ -3,7 +3,7 @@ object Euler34 extends Application {
   def digfact (sum :Int, n :Int) :Int =
     if (n == 0) sum else digfact(facts(n % 10) + sum, n/10);
   def check (sum :Int, n :Int) :Int =
-    if (n == 9999999) sum;
+    if (n == 2000000) sum;
     else if (digfact(0, n) == n) check(sum+n, n+1);
     else check(sum, n+1);
   println(check(0, 10));
