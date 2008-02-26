1a8c36e77fcc41768eb7f7581e10f20dc550aa8e
Michael Bayne <mdb@samskivert.com>
2008-02-26 07:48:56 +0000

    Pre-blog tidying.

--- Euler38.scala
+++ Euler38.scala
@@ -1,6 +1,8 @@
 object Euler38 extends Application {
+  def ispan (n :String) = n.toList.sort(_<_).mkString == "123456789";
+  println(List.range(9182,9876).reverse.map(n => n+""+(2*n)).filter(ispan).head);
+  // or
   def catprod (n :Int, r :Int) = List.range(1, r+1).flatMap(p => (n*p).toString.toList)
-  def ispan (n :List[Char]) = n.sort(_<_).mkString == "123456789";
   def find (n :Int, r :Int, max :Int) :Int = {
     val cp = catprod(n, r);
     if (cp.length > 9)
