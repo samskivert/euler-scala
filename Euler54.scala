58cd77934ab2e6f6523b4d4609dc747e2c5e177e
Michael Bayne <mdb@samskivert.com>
2008-03-03 00:21:53 +0000

    Nixed unneeded import. Ha!

--- Euler54.scala
+++ Euler54.scala
@@ -1,5 +1,4 @@
 import scala.io.Source;
-import scala.runtime.RichString;
 
 object Euler54 extends Application {
   val ranks = List('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
