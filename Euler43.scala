cbcad629176e408925e0356bb6c1014a3167ece0
Michael Bayne <mdb@samskivert.com>
2008-06-01 19:07:39 +0000

    Brevifying.

--- Euler43.scala
+++ Euler43.scala
@@ -1,13 +1,12 @@
 object Euler43 extends Application {
-  def perms (d :List[Char], n :List[Char]) :List[String] = {
-    if (d == Nil) n.mkString :: Nil;
-    else d.flatMap(digit => perms(d.filter(digit.!=), digit :: n))
+  def perms (d :List[Char], n :List[Char]) :List[String] = d match {
+    case Nil => n.mkString :: Nil;
+    case _ => d.flatMap(digit => perms(d.filter(digit.!=), digit :: n))
   }
-  def checkmod (n :String, off :Int, d :Int) :Boolean = (n.substring(off, off+3).toInt % d == 0);
+  def checkmod (n :String, off :Int, d :Int) = (n.substring(off, off+3).toInt % d == 0);
   def ismatch (n :String) :Boolean = {
     checkmod(n, 1, 2) && checkmod(n, 2, 3) && checkmod(n, 3, 5) && checkmod(n, 4, 7) &&
     checkmod(n, 5, 11) && checkmod(n, 6, 13) && checkmod(n, 7, 17)
   }
-  val matches = perms("0123456789".toList, Nil).filter(ismatch);
-  println(matches.map(BigInt.apply).foldRight(BigInt(0))(_+_))
+  println(perms("0123456789".toList, Nil).filter(ismatch).map(BigInt.apply).reduceLeft(_+_))
 }
