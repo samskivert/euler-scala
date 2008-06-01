1b6d81914038b1d4352ca831d91857fd9cf426da
Michael Bayne <mdb@samskivert.com>
2008-06-01 19:07:09 +0000

    Discovered new sneaky business that simplifies solution.

--- Euler42.scala
+++ Euler42.scala
@@ -1,11 +1,5 @@
 object Euler42 extends EulerApp {
-  val words = readwords("words.txt");
-  def wvalue (name :String) = name.foldLeft(0)((s, l) => (s + (l - 'A' + 1)))
-  def istri (value :Int, n :Int) :Int = {
-    val ntri = (n*(n+1))/2;
-    if (ntri == value) return 1;
-    else if (ntri > value) return 0;
-    else return istri(value, n+1);
-  }
-  println(words.foldLeft(0)((count, word) => (count + istri(wvalue(word), 1))));
+  def wvalue (word :Seq[Char]) = word.foldLeft(0)((s, l) => (s + (l - 'A' + 1)))
+  def tri (n :Int) = (n*(n+1))/2
+  println(readwords("words.txt").map(wvalue).filter(v => (v == tri(Math.sqrt(2*v)))).length);
 }
