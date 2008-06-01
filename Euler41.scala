856ca823b38cb35da7bba04e6a6ef89fad520e21
Michael Bayne <mdb@samskivert.com>
2008-06-01 18:12:18 +0000

    Use a case method and foldl.

--- Euler41.scala
+++ Euler41.scala
@@ -1,7 +1,7 @@
 object Euler41 extends EulerApp {
-  def perms (d :List[Char], n :List[Char]) :List[Int] = {
-    if (d == Nil) n.mkString.toInt :: Nil;
-    else d.flatMap(digit => perms(d.filter(digit.!=), digit :: n))
+  def perms (d :List[Char], n :List[Char]) :List[Int] = d match {
+    case Nil => n.mkString.toInt :: Nil;
+    case _ => d.flatMap(digit => perms(d.filter(digit.!=), digit :: n))
   }
-  println(perms("1234567".toList, Nil).filter(isprime).foldRight(0)(Math.max));
+  println(perms("1234567".toList, Nil).filter(isprime).foldLeft(0)(Math.max));
 }
