fa60740e365b422c80e79ea33d51d576a2b1ab0b
Michael Bayne <mdb@samskivert.com>
2008-07-01 14:18:29 +0000

    Nixed some unused bits.

--- Euler71.scala
+++ Euler71.scala
@@ -1,13 +1,7 @@
 object Euler71 extends EulerApp {
   case class Frac (numer :Int, denom :Int) {
     def lessThan (o :Frac) = numer*o.denom < o.numer*denom
-    def reduce = {
-      val div = gcd(numer, denom)
-      Frac(numer/div, denom/div)
-    }
-    def delta = 3d/7 - numer.toDouble/denom
   }
-
-  val fracs = 3.to(1000000).map(n => Frac(3*n/7, n)).filter(_.lessThan(Frac(3, 7))).toList
+  val fracs = 7.to(1000000).map(n => Frac(n*3/7, n)).filter(_.lessThan(Frac(3, 7))).toList
   println(fracs.sort(_.lessThan(_)).last)
 }
