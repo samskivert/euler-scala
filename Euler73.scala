f7f8f7f030a21fb6f88fcad09a7a1c7b2ee2ebf4
Michael Bayne <mdb@samskivert.com>
2008-07-20 16:23:16 +0000

    Brevification.

--- Euler73.scala
+++ Euler73.scala
@@ -4,6 +4,5 @@ object Euler73 extends EulerApp {
     val pfacts = primefacts(primes, denom)
     (numer :Int) => !pfacts.exists(p => (numer%p)==0)
   }
-  def irrfracs (d :Int) = ((d.toFloat/3).ceil.toInt).to(d/2).filter(iscoprime(d)).length
-  println(4.to(10000).map(irrfracs).sum)
+  println(4.to(10000).map(d => (d/3+1).to(d/2).filter(iscoprime(d)).length).sum)
 }
