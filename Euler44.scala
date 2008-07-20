fd29621c21cb06f6e9aeb89f42282f59f42b6703
Michael Bayne <mdb@samskivert.com>
2008-07-20 11:58:03 +0000

    Minor reformatting.

--- Euler44.scala
+++ Euler44.scala
@@ -1,14 +1,14 @@
 object Euler44 extends Application {
-  def pent (n :Int) = n*(3*n-1)/2;
-  val pents = Set() ++ List.range(1, 3000).map(pent);
+  def pent (n :Int) = n*(3*n-1)/2
+  val pents = Set() ++ List.range(1, 3000).map(pent)
   def find (a :Int, b :Int) :Int = {
-    val pa = pent(a);
-    val pb = pent(b);
-    val d = pb-pa;
-    val s = pa+pb;
-    if (pents.contains(d) && pents.contains(s)) return d;
-    if (a >= b) return find(1, b+1);
-    else return find(a+1, b);
+    val pa = pent(a)
+    val pb = pent(b)
+    val d = pb-pa
+    val s = pa+pb
+    if (pents.contains(d) && pents.contains(s)) d
+    else if (a >= b) find(1, b+1)
+    else find(a+1, b)
   }
-  println(find(1, 2));
+  println(find(1, 2))
 }
