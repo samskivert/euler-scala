008b9fc630e356db13d6b45f19b14512a8431a83
Michael Bayne <mdb@samskivert.com>
2008-07-20 11:41:40 +0000

    Reworked the solution as the original brute force solution was way too slow and memory intensive.

--- Euler43.scala
+++ Euler43.scala
@@ -1,12 +1,11 @@
 object Euler43 extends Application {
-  def perms (d :List[Char], n :List[Char]) :List[String] = d match {
-    case Nil => n.mkString :: Nil;
-    case _ => d.flatMap(digit => perms(d.filter(digit.!=), digit :: n))
+  def compsum (n :Long, digits :List[Int], divs :List[Int], sum :Long) :Long = {
+    if (digits.isEmpty) n + sum
+    else (for { d <- digits; val nn = n*10 + d; if ((nn%1000) % divs.head == 0) }
+          yield compsum(nn, digits-d, divs.tail, sum)).foldLeft(0L)(_+_)
   }
-  def checkmod (n :String, off :Int, d :Int) = (n.substring(off, off+3).toInt % d == 0);
-  def ismatch (n :String) :Boolean = {
-    checkmod(n, 1, 2) && checkmod(n, 2, 3) && checkmod(n, 3, 5) && checkmod(n, 4, 7) &&
-    checkmod(n, 5, 11) && checkmod(n, 6, 13) && checkmod(n, 7, 17)
-  }
-  println(perms("0123456789".toList, Nil).filter(ismatch).map(BigInt.apply).reduceLeft(_+_))
+  def digits (n :Int) = n.toString.toList.map(_-'0')
+  def norepeats (n :Int) = digits(n).removeDuplicates == digits(n)
+  def compute (s :Int) = compsum(s, 0.to(9).toList -- digits(s), List(2, 3, 5, 7, 11, 13, 17), 0)
+  println(100.to(999).filter(norepeats).map(compute).reduceLeft(_+_))
 }
