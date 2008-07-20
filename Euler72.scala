b0fac7f48c969d40c578ec960778cc2a3930591c
Michael Bayne <mdb@samskivert.com>
2008-07-20 15:59:10 +0000

    We can use reduce in this case.

--- Euler72.scala
+++ Euler72.scala
@@ -1,5 +1,5 @@
 object Euler72 extends EulerApp {
   val primes = genprimes(Math.sqrt(1000000)).filter(0.!=)
   def phi (n :Int) = primefacts(primes, n).foldLeft(n.toLong)((b, a) => (b * (a-1) / a))
-  println(2.to(1000000).map(phi).foldLeft(0L)(_+_))
+  println(2.to(1000000).map(phi).reduceLeft(_+_))
 }
