da0413f8b1fe5413463d44ebc5c61f6aa895ab53
Michael Bayne <mdb@samskivert.com>
2008-07-21 16:52:53 +0000

    Better names.

--- Euler74.scala
+++ Euler74.scala
@@ -2,17 +2,17 @@ object Euler74 {
   val facts = Array(1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880);
   def sumfact (n :Int) :Int = if (n == 0) 0 else sumfact(n/10) + facts(n%10)
 
-  val lens = new scala.collection.mutable.HashMap[Int,Int]
-  def notelens (chain :List[Int], length :Int) :Int = {
-    lens.put(chain.head, length)
-    if (chain.tail.isEmpty) length else notelens(chain.tail, length+1)
+  val cache = new scala.collection.mutable.HashMap[Int,Int]
+  def memoize (chain :List[Int], length :Int) :Int = {
+    cache.put(chain.head, length)
+    if (chain.tail.isEmpty) length else memoize(chain.tail, length+1)
   }
 
   def compute (chain :List[Int]) :Int = {
     val next = sumfact(chain.head)
-    val memo = lens.get(next)
-    if (memo.isDefined) notelens(chain, memo.get+1)
-    else if (chain.indexOf(next) >= 0) notelens(chain, 1)
+    val memo = cache.get(next)
+    if (memo.isDefined) memoize(chain, memo.get+1)
+    else if (chain.indexOf(next) >= 0) memoize(chain, 1)
     else compute(next :: chain)
   }
 
