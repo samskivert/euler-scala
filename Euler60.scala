bae95d2e662b81f64097b715cfef69c3d54ab770
Michael Bayne <mdb@samskivert.com>
2008-08-03 23:54:55 +0000

    New much faster solution.

--- Euler60.scala
+++ Euler60.scala
@@ -1,22 +1,21 @@
 object Euler60 extends EulerApp {
   def primepair (p1 :Int, p2 :Int) =
-    isprime((p1.toString + p2).toInt) && isprime((p2.toString + p1).toInt)
+    isprime((p1.toString+p2).toInt) && isprime((p2.toString+p1).toInt)
 
-  val plist = genprimes(2000).filter(0.!=)
-  val pairs = new scala.collection.mutable.HashSet[String]()
+  val plist = genprimes(10000).filter(0.!=)
+  val ppairs = new scala.collection.mutable.HashSet[Int]
   for (ii <- 0.until(plist.length-1); jj <- ii.until(plist.length))
     if (primepair(plist(ii), plist(jj)))
-      pairs += (plist(ii)+"."+plist(jj))
-  println(pairs.size)
+      ppairs += (plist(ii)*10000 + plist(jj))
 
-  def ppair (p1 :Int, p2 :Int) = pairs(p1+"."+p2)
+  def isset (pset :List[Int], prime :Int) :Boolean =
+    pset.isEmpty || (ppairs(pset.head*10000+prime) && isset(pset.tail, prime))
+  def find (pset :List[Int], plist :List[Int]) :Option[List[Int]] = {
+    if (pset.size == 5) Some(pset)
+    else if (plist.isEmpty) None
+    else if (!isset(pset, plist.head)) find(pset, plist.tail)
+    else find(plist.head :: pset, plist.tail).orElse(find(pset, plist.tail))
+  }
 
-  def search (primes :Array[Int], list :List[Int], idx :Int) :List[Int] =
-    if (list.length == 4) list
-    else if (idx == primes.length) Nil
-    else if (list.forall(ppair(_, primes(idx)))) search(primes, list+primes(idx), idx+1)
-    else search(primes, list, idx+1)
-
-  val sols = 0.until(plist.length).map(idx => search(plist, List(plist(idx)), idx+1))
-  println(sols.filter(Nil.!=));
+  println(find(Nil, plist.toList).get.sum)
 }
