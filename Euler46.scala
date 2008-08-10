80e66bca221d0291e11980a9c3f9545892425ea6
Michael Bayne <mdb@samskivert.com>
2008-08-10 17:56:44 +0000

    Tiding up.

--- Euler46.scala
+++ Euler46.scala
@@ -1,6 +1,5 @@
 object Euler46 extends EulerApp {
-  def isgold (n :Int) :Boolean =
-    List.range(1, Math.sqrt(n/2).toInt+1).exists(s => isprime(n-2*s*s));
-  def check (n :Int) :Int = if (isprime(n) || isgold(n)) check(n+2) else n;
-  println(check(3));
+  def isgold (n :Int) = 1.to(Math.sqrt(n/2)).exists(s => isprime(n-2*s*s))
+  def check (n :Int) :Int = if (isprime(n) || isgold(n)) check(n+2) else n
+  println(check(3))
 }
