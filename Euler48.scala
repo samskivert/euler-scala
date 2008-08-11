9eec86b6d2e88ddb79c80e8be78cc85722860095
Michael Bayne <mdb@samskivert.com>
2008-08-11 15:53:03 +0000

    Tidying up.

--- Euler48.scala
+++ Euler48.scala
@@ -1,4 +1,3 @@
 object Euler48 extends Application {
-  val modulo = BigInt(10).pow(10);
-  println(List.range(1, 1001).map(a => BigInt(a).pow(a)).foldRight(BigInt(0))(_+_) % modulo);
+  println(1.to(1000).map(a => BigInt(a).pow(a)).reduceRight(_+_) % BigInt(10).pow(10))
 }
--- Euler49.scala
+++ Euler49.scala
@@ -1,11 +1,7 @@
 object Euler49 extends EulerApp {
-  def sort (n :Int) = n.toString.toList.sort(_<_).mkString.toInt;
-  for (n <- List.range(1488, 10000-2*3330)) {
-    val n1 = n+3330;
-    val n2 = n+2*3330;
-    if (isprime(n) && isprime(n1) && isprime(n2) &&
-        sort(n1) == sort(n) && sort(n2) == sort(n)) {
-      println("" + n + n1 + n2);
-    }
-  }
+  def sort (n :Int) = n.toString.toList.sort(_<_).mkString.toInt
+  def ismem (n :Int, nn :Int) = isprime(nn) && sort(nn) == sort(n)
+  def isseq (n :Int) = ismem(n, n+3330) && ismem(n, n+6660)
+  def mkseq (n :Int) = List(n, n+3330, n+6660).mkString
+  println(mkseq(1488.to(10000-6660).filter(isprime).filter(isseq).first))
 }
