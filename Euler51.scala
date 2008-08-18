94532020e787f4a81273b411dd505be5174ddaf9
Michael Bayne <mdb@samskivert.com>
2008-08-18 16:56:25 +0000

    More tidying.

--- Euler51.scala
+++ Euler51.scala
@@ -1,7 +1,7 @@
 object Euler51 extends EulerApp {
   def cprimes (n :String)(d :Char) = {
     val variants = "0123456789".toList.map(nd => n.replace(d, nd))
-    variants.filter(n => !n.startsWith("0")).map(Integer.parseInt).filter(isprime).length
+    variants.filter(!_.startsWith("0")).map(Integer.parseInt).filter(isprime).length
   }
   def count (prime :String) = {
     prime.toList.removeDuplicates.map(cprimes(prime)).reduceRight(Math.max)
