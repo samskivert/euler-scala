405d26d69c60884c68c96211ed28d1158d9717f5
Michael Bayne <mdb@samskivert.com>
2009-01-27 18:38:40 -0800

    About a zillion times faster owing mostly to using locals instead of class members.

--- Euler78.scala
+++ Euler78.scala
@@ -1,32 +1,19 @@
 object Euler78 extends EulerApp {
-  val parts = Array.make(1000000, BigInt(0))
-  parts(0) = 1
-  def part (n :Int) :BigInt = {
-    if (n < 0) 0
-    else if (parts(n) > 0) parts(n)
-    else {
-      var pn = BigInt(0)
-      for (k <- 1.to(Math.sqrt(n))) {
-        val p1 = part(n - k * (3*k - 1) / 2)
-        val p2 = part(n - k * (3*k + 1) / 2)
-        if (k % 2 == 1) pn = pn + p1 + p2
-        else pn = pn - p1 - p2
+  def part_mod (n :Int, parts :Array[Int]) = {
+    var sign = 1
+    for (k <- 1 to Math.sqrt(n)) {
+      val n1 = k * (3*k - 1) / 2
+      if (n1 <= n) {
+        parts(n) = (parts(n) + sign * parts(n-n1))
+        val n2 = k * (3*k + 1) / 2
+        if (n2 <= n) parts(n) = (parts(n) + sign * parts(n-n2))
       }
-      parts(n) = pn
-      pn
+      sign = -sign
     }
+    parts(n) %= 1000000
+    parts(n) == 0
   }
-
-  var n = 0
-  while ((part(n) % 1000000) != 0) {
-    n = n + 1
-    if (n % 100 == 0) {
-      println(n + " -> " + part(n))
-    }
-  }
-  println(n)
-
-//   for (n <- 1.to(500)) {
-//     println(n + " -> " + part(n))
-//   }
+  val parts = Array.make(100000, 0)
+  parts(0) = 1
+  println(Iterator.from(1).find(part_mod(_, parts)).get)
 }
