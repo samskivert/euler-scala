9d0b3e01b6fac5525a0dc26018f23d090a43c14e
Michael Bayne <mdb@samskivert.com>
2009-12-16 01:10:25 -0800

    Better solution.

--- Euler79.scala
+++ Euler79.scala
@@ -1,16 +1,11 @@
 object Euler79 extends EulerApp {
-  def matchp (c1 :Char, c2 :Char)(code :String) = c1 == code(0) && c2 == code(2)
-  def find (codes :List[String], code :String, pos :Int) :String =
-    if (codes.length == 0) code
-    else codes.find(matchp(code(pos), code(pos+1))) match {
-      case None =>
-        if (pos < code.length-2) find(codes, code, pos+1) else code
-      case Some(c) =>
-        find(codes.filterNot(_ == c), code.patch(pos+1, c.slice(1, 2), 0), pos)
+  def pop (fc :Char)(c :String) = if (c.head == fc) c.tail else c
+  def find (codes :List[String], code :String) :String = {
+    val (fs, ss) = codes.unzip(s => (s(0), s.drop(1)))
+    (fs.toSet -- ss.flatMap(_.toList).toSet).headOption match {
+      case None => code
+      case Some(fc) => find(codes.map(pop(fc)).filterNot(_.isEmpty), code + fc)
     }
-  val codes = readlines("keylog.txt").removeDuplicates
-  val (fst, snd, thd) = codes.foldLeft(Set[Char](), Set[Char](), Set[Char]())(
-    (s, b) => (s._1+b(0), s._2+b(1), s._3+b(2)))
-  val middle = find(codes.drop(1), codes.head, 0)
-  println((fst -- (snd ++ thd)).head + middle + (thd -- (fst ++ snd)).head)
+  }
+  println(find(readlines("keylog.txt"), ""))
 }
