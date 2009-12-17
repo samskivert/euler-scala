8003674c1c07b4ae1155f19e54c9fe27781e390e
Michael Bayne <mdb@samskivert.com>
2009-12-16 17:00:18 -0800

    Solution for problem 80.

---
+++ Euler80.scala
@@ -0,0 +1,12 @@
+object Euler80 extends EulerApp {
+  def f (p :BigInt, x :Int) = (p*20 + x) * x
+  def maxx (c :BigInt, p :BigInt, x :Int) :Int = if (f(p, x) > c) x-1 else maxx(c, p, x+1)
+  def root (c :BigInt, p :BigInt, acc :Int, iter :Int) :Int = {
+    val x = maxx(c, p, 1)
+    val y = f(p, x)
+    if (y == c) 0
+    else if (iter == 99) acc+x
+    else root((c - y) * 100, p*10 + x, acc+x, iter+1)
+  }
+  println((2 to 100).map(n => root(n, 0, 0, 0)).sum)
+}
