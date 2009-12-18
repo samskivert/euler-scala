8bd1935c1f71a61ba560490aaf7e2a2be49c9ca7
Michael Bayne <mdb@samskivert.com>
2009-12-18 09:02:49 -0800

    Brevified.

--- Euler81.scala
+++ Euler81.scala
@@ -1,8 +1,8 @@
 object Euler81 extends EulerApp {
   val matrix = readlines("matrix.txt").map(_.split(",").map(_.toInt))
-  for (y <- 1.until(matrix.length)) matrix(y)(0) += matrix(y-1)(0)
-  for (x <- 1.until(matrix.length)) matrix(0)(x) += matrix(0)(x-1)
-  for (y <- 1.until(matrix.length); x <- 1.until(matrix.length))
+  for (y <- 1 until matrix.length) matrix(y)(0) += matrix(y-1)(0)
+  for (x <- 1 until matrix.length) matrix(0)(x) += matrix(0)(x-1)
+  for (y <- 1 until matrix.length; x <- 1 until matrix.length)
     matrix(y)(x) += Math.min(matrix(y-1)(x), matrix(y)(x-1))
   println(matrix.last.last)
 }
