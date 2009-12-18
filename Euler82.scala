2613c2bc25304325a9d8a2f09694d543dac5b180
Michael Bayne <mdb@bering.cs.washington.edu>
2009-12-18 13:00:26 -0800

    More fixy, figured out the problem but it's not implemented properly.

--- Euler82.scala
+++ Euler82.scala
@@ -1,22 +1,24 @@
 object Euler82 extends EulerApp {
-  val matrix = readlines("matrix.txt").map(_.split(",").map(_.toInt))
-  def slice (m :Int) = List.range(0, matrix.length).map(n => matrix(n)(m))
+  val matrix = readlines("mmatrix.txt").map(_.split(",").map(_.toInt))
+  def slice (m :Int) = List.range(0, matrix.length).map(n => matrix(n)(m)).toArray
 
-  def cheapest (accum :List[Int], cur :List[Int]) = {
-    (accum(0)+cur(0)) :: List.range(1, matrix.length).map(n => cur(n)+Math.min(accum(n), cur(n-1)))
-  }
-
-  def iter (accum :List[Int], cur :List[Int]) = {
-    // println(accum)
-    // println(cur)
-    var across = accum.zip(cur).map(p => p._1+p._2)
-    var down = across(0) :: List.range(1, across.length).map(n => Math.min(across(n-1)+cur(n), across(n)))
-    List.range(0, across.length-1).map(n => Math.min(across(n+1)+cur(n), down(n))) ::: List(across.last)
+  def iter (accum :Array[Int], cur :Array[Int]) = {
+    var col = accum.zip(cur).map(p => p._1+p._2)
+    println("1 -> " + col.toList)
+    for (y <- 1 until col.length) {
+      col(y) = Math.min(col(y), col(y-1)+col(y))
+    }
+    println("2 -> " + col.toList)
+    for (y <- (col.length-2) until(0, -1)) {
+      col(y) = Math.min(col(y), col(y+1)+col(y))
+    }
+    println("3 -> " + col.toList)
+    col
   }
 
   var accum = slice(0)
+  println(accum.toList)
   for (n <- 1 until matrix.length) {
-    // println(accum)
     accum = iter(accum, slice(n))
   }
   println(accum.min)
