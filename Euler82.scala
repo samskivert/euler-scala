dd553543a73b171c4c700098689ca5fd98e91b68
Michael Bayne <mdb@mandevilla.gateway.2wire.net>
2009-12-19 16:06:55 -0800

    Correct and cleaned up solution to 82.

--- Euler82.scala
+++ Euler82.scala
@@ -1,25 +1,10 @@
 object Euler82 extends EulerApp {
-  val matrix = readlines("mmatrix.txt").map(_.split(",").map(_.toInt))
-  def slice (m :Int) = List.range(0, matrix.length).map(n => matrix(n)(m)).toArray
-
-  def iter (accum :Array[Int], cur :Array[Int]) = {
-    var col = accum.zip(cur).map(p => p._1+p._2)
-    println("1 -> " + col.toList)
-    for (y <- 1 until col.length) {
-      col(y) = Math.min(col(y), col(y-1)+col(y))
-    }
-    println("2 -> " + col.toList)
-    for (y <- (col.length-2) until(0, -1)) {
-      col(y) = Math.min(col(y), col(y+1)+col(y))
-    }
-    println("3 -> " + col.toList)
-    col
+  val matrix = readlines("matrix.txt").map(_.split(",").map(_.toInt)).transpose
+  def iter (accum :List[Int], cur :List[Int]) = {
+    var col = accum.zip(cur).map(p => p._1+p._2).toArray
+    for (y <- 1 until col.length) col(y) = Math.min(col(y), col(y-1)+cur(y))
+    for (y <- (0 until col.length-1).reverse) col(y) = Math.min(col(y), col(y+1)+cur(y))
+    col.toList
   }
-
-  var accum = slice(0)
-  println(accum.toList)
-  for (n <- 1 until matrix.length) {
-    accum = iter(accum, slice(n))
-  }
-  println(accum.min)
+  println((matrix reduceLeft iter).min)
 }
