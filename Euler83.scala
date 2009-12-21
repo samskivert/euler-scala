70247ff550e85926159d098e1dbb6ac2e020c7fd
Michael Bayne <mdb@samskivert.com>
2009-12-20 22:40:11 -0800

    Solution to 83 using Dijkstra's algorithm.

--- Euler83.scala
+++ Euler83.scala
@@ -1,32 +1,24 @@
+import scala.collection.immutable.TreeSet
 object Euler83 extends EulerApp {
-  private var _iter :Int = 0
-  def print (matrix :Array[Array[Int]]) {
-    for (y <- 0 until matrix.length) {
-      for (x <- 0 until matrix.length) {
-        printf("%4d ", matrix(y)(x))
+  val deltas = List((-1,0),(0,-1),(1,0),(0,1))
+  val matrix = readlines("matrix.txt").map(_.split(",").map(_.toInt)).toArray
+  val costs = Array.fill(matrix.length, matrix.length)(Int.MaxValue)
+  costs(0)(0) = matrix(0)(0)
+  var nodes = TreeSet((costs(0)(0), 0, 0))
+  while (!nodes.isEmpty) {
+    val node = nodes.head
+    nodes -= node
+    for ((nx, ny) <- deltas.map(p => (node._2+p._1, node._3+p._2))) {
+      if (ny >= 0 && ny < matrix.length && nx >= 0 && nx < matrix.length) {
+        val nc = node._1 + matrix(ny)(nx)
+        val cc = costs(ny)(nx)
+        if (cc > nc) {
+          nodes -= Tuple3(cc, nx, ny)
+          costs(ny)(nx) = nc
+          nodes += Tuple3(nc, nx, ny)
+        }
       }
-      println("")
     }
   }
-  def search (matrix :Array[Array[Int]], cost :Array[Array[Int]], x :Int, y :Int, nc :Int) {
-    val c = cost(y)(x)
-    if (c != 0 && nc >= c) {
-      return
-    }
-    if (c != 0) {
-      println("Lowering " + x + "/" + y + " to " + nc + " from " + c)
-    }
-    cost(y)(x) = nc
-    _iter += 1
-    if (x > 0) search(matrix, cost, x-1, y, nc + matrix(y)(x-1))
-    if (y > 0) search(matrix, cost, x, y-1, nc + matrix(y-1)(x))
-    if (x < matrix.length-1) search(matrix, cost, x+1, y, nc + matrix(y)(x+1))
-    if (y < matrix.length-1) search(matrix, cost, x, y+1, nc + matrix(y+1)(x))
-  }
-  val matrix = readlines("mmatrix.txt").map(_.split(",").map(_.toInt)).toArray
-  val cost = Array.ofDim[Int](matrix.length, matrix.length)
-  search(matrix, cost, 0, 0, matrix(0)(0))
-  print(cost)
-  println(cost.last.last)
-  println(_iter)
+  println(costs.last.last)
 }
