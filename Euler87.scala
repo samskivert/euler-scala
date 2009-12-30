fc8cd9244fce6f2d110b8363c6e72d2cf4f80681
Michael Bayne <mdb@mandevilla.local>
2009-12-29 19:57:49 -0800

    Fixed (needed to remove dups), switched to shorter format.

--- Euler87.scala
+++ Euler87.scala
@@ -1,11 +1,8 @@
 object Euler87 extends EulerApp {
-  val limit = 50000000
-  val primes = genprimes(2*Math.sqrt(limit).toInt).filter(_!=0).toArray
-  def f (a :Int, b :Int, c :Int) = a*a + b*b*b + c*c*c*c
-  def count (cnt :Int, ai :Int, bi :Int, ci :Int) :Int =
-    if (f(primes(ai), primes(bi), primes(ci)) < limit) count(cnt+1, ai+1, bi, ci)
-    else if (f(primes(0), primes(bi+1), primes(ci)) < limit) count(cnt+1, 1, bi+1, ci)
-    else if (f(primes(0), primes(0), primes(ci+1)) < limit) count(cnt+1, 1, 0, ci+1)
-    else cnt
-  println(count(0, 0, 0, 0))
+  val primes = genprimes(7071).filter(_!=0)
+  val ns = for (a <- primes; // 7071 = 2rt(50m)
+                b <- primes.takeWhile(_ < 368); // 3rt(50m)
+                c <- primes.takeWhile(_ < 84); // 4rt(50m)
+                n = a*a + b*b*b + c*c*c*c; if (n < 50000000)) yield n
+  println(ns.removeDuplicates.length)
 }
