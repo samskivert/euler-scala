22e9bcbc936734974b8cff52c220657df44053ca
Michael Bayne <mdb@mandevilla.local>
2009-12-31 19:39:44 -0800

    Seriously premature optimization. Same algorithm, just nixed a bunch of function calls and removed my 10x more expensive "caching" of the sqrt.

--- Euler86.scala
+++ Euler86.scala
@@ -1,21 +1,12 @@
-import scala.collection.mutable.Map
 object Euler86 extends EulerApp {
-  def hyp (x :Int, y :Int) = x*x + y*y
-  def minpath (h :Int, w :Int, d :Int) = Math.min(hyp(h+w,d), Math.min(hyp(h+d,w), hyp(w+d,h)))
-  def isint (y :Double) = if (y - y.toInt == 0) 1 else 0
-  def count (h :Int, w :Int, d :Int, c :Int, cache :Map[Int,Int]) :Int = {
-    val mp = minpath(h, w, d)
-    val nc = c + cache.getOrElseUpdate(mp, isint(Math.sqrt(mp)))
-    if (d == h) {
-      if (w == h) nc
-      else count(h, w+1, w+1, nc, cache)
-    } else count(h, w, d+1, nc, cache)
+  def search (target :Int, h :Int, c :Int) :Int = {
+    var nc = c
+    for (w <- 1 to h; d <- w to h) {
+      val mp = Math.sqrt((w+d)*(w+d) + h*h)
+      if (mp - mp.toInt == 0) nc += 1
+    }
+    if (nc > target) h
+    else search(target, h+1, nc)
   }
-  def search (h :Int, c :Int, target :Int, cache :Map[Int,Int]) :Int = {
-    println(h + " -> " + c)
-    if (c > target) h-1
-    else search(h+1, count(h, 1, 1, c, cache), target, cache)
-  }
-  println(search(1, 0, 2000, Map[Int,Int]()))
-  println(search(1, 0, 1000000, Map[Int,Int]()))
+  println(search(1000000, 1, 0))
 }
