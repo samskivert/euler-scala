2c570c340fc6af3df55b479410d6f89e83fe0b94
Michael Bayne <mdb@mandevilla.local>
2010-01-01 17:26:35 -0800

    Use fancy formatting for code.

--- blog.pl
+++ blog.pl
@@ -3,12 +3,12 @@
 my $number = shift or die "Usage: $0 N\n";
 my $url = "http://projecteuler.net/index.php";
 printf("<a href=\"$url?section=problems&id=%d\">Problem %03d</a>:\n", $number, $number);
-print("<pre>\n");
+print("[scala]\n");
 open(CODE, "Euler$number.scala") or die "Can't read Euler$number.scala: $!\n";
 while (<CODE>) {
     $_ =~ s#<#\&lt;#g;
-    $_ =~ s#EulerApp#<a href="/code/euler/EulerApp.scala">EulerApp</a>#g;
+    # $_ =~ s#EulerApp#<a href="/code/euler/EulerApp.scala">EulerApp</a>#g;
     print $_;
 }
 # print(`sed '' Euler$number.scala`);
-print("</pre>\n");
+print("[/scala]\n");
