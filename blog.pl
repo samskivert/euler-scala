659926340731166af3fd3feec8b113b0c1252773
Michael Bayne <mdb@mandevilla.local>
2010-01-01 17:51:43 -0800

    Don't need to escape < in [scala] blocks.

--- blog.pl
+++ blog.pl
@@ -6,7 +6,7 @@ printf("<a href=\"$url?section=problems&id=%d\">Problem %03d</a>:\n", $number, $
 print("[scala]\n");
 open(CODE, "Euler$number.scala") or die "Can't read Euler$number.scala: $!\n";
 while (<CODE>) {
-    $_ =~ s#<#\&lt;#g;
+    # $_ =~ s#<#\&lt;#g;
     # $_ =~ s#EulerApp#<a href="/code/euler/EulerApp.scala">EulerApp</a>#g;
     print $_;
 }
