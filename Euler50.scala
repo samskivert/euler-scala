6a924e2cffdb4cd0add813c1ecbc2d4fd0166efe
Michael Bayne <mdb@mandevilla.local>
2010-01-01 17:26:09 -0800

    Slight rearrangement.

--- Euler50.scala
+++ Euler50.scala
@@ -1,9 +1,9 @@
 object Euler50 extends EulerApp {
-  val pvec = genprimes(1000000)
-  val primes = pvec.filter(0.!=)
   case class PSum (sum :Int, length :Int) {
     def add (prime :Int) = PSum(sum+prime, length+1)
   }
+  val pvec = genprimes(1000000)
+  val primes = pvec.filter(0.!=)
   def fsum (idx :Int, csum :PSum, lsum :PSum) :PSum = {
     if (idx >= primes.length || csum.sum >= pvec.length) lsum
     else fsum(idx+1, csum.add(primes(idx)), if (pvec(csum.sum) != 0) csum else lsum)
