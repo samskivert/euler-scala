3d09d444c96e4a300d8c2afbe1d5bad343131ca7
Michael Bayne <mdb@mandevilla.local>
2010-01-01 19:24:54 -0800

    Cleaned up for posting.

--- Euler53.scala
+++ Euler53.scala
@@ -1,5 +1,5 @@
 object Euler53 extends Application {
-  def fact (n :BigInt) :BigInt = if (n < 2) 1 else n * fact(n-1);
-  def choose (n :BigInt, r :BigInt) = fact(n)/(fact(r)*fact(n-r));
+  def fact (n :BigInt) :BigInt = if (n < 2) 1 else n * fact(n-1)
+  def choose (n :BigInt, r :BigInt) = fact(n)/(fact(r)*fact(n-r))
   println((for (n <- 1 to 100; r <- 1 to n; if (choose(n, r) > 1000000)) yield 1).sum)
 }
--- Euler55.scala
+++ Euler55.scala
@@ -1,10 +1,8 @@
-object Euler55 extends Application {
-  def ispan (n :String) =
-    n.substring(0, n.length/2) == n.substring(n.length-n.length/2).reverse.mkString;
-  def islychrel (n :BigInt, iter :Int) :Boolean = {
-    if (iter > 0 && ispan(n.toString)) true;
-    else if (iter == 50) false;
-    else islychrel(n + BigInt(n.toString.reverse.mkString), iter+1);
-  }
-  println(List.range(1, 10000).filter(n => !islychrel(n, 0)).length);
+object Euler55 extends EulerApp {
+  def ispal (n :String) = n.take(n.length/2) == n.takeRight(n.length/2).reverse
+  def islychrel (n :BigInt, iter :Int = 0) :Int =
+    if (iter > 0 && ispal(n.toString)) 0
+    else if (iter == 50) 1
+    else islychrel(n + BigInt(n.toString.reverse), iter+1)
+  println((1 to 9999).map(n => islychrel(n)).sum)
 }
