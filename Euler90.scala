2de98bb9c964a4b7a34cbeb7bb2f238b4ff0b1cc
Michael Bayne <mdb@mandevilla.local>
2010-01-02 22:06:48 -0800

    Less retarded way of enumerating 10 choose 6. Other tidying.

--- Euler90.scala
+++ Euler90.scala
@@ -1,11 +1,8 @@
 object Euler90 extends EulerApp {
-  def augment (cube :Set[Int]) = if (cube(6)) cube+9 else if (cube(9)) cube+6 else cube
-  def mkcubes (from :Seq[Int], s :Int, n :Int) :Seq[Set[Int]] = {
-    if (n == 0) List(augment(from toSet))
-    else s to 9 flatMap(d => mkcubes(from filter d.!=, Math.max(d, s)+1, n-1))
-  }
+  def augment (cube :Set[Int]) = if (cube(6) || cube(9)) cube+6+9 else cube
+  val cubes = for (a <- 0 to 9; b <- a+1 to 9; c <- b+1 to 9; d <- c+1 to 9;
+                   e <- d+1 to 9; f <- e+1 to 9) yield augment(Set(a, b, c, d, e, f))
   def valid (c1 :Set[Int], c2 :Set[Int]) =
     1 to 9 map(n => n*n) forall(n => c1(n/10) && c2(n%10) || c2(n/10) && c1(n%10))
-  val cubes = mkcubes(0 to 9, 0, 4)
   println((for (c1 <- cubes; c2 <- cubes; if valid(c1, c2)) yield 1).sum / 2)
 }
