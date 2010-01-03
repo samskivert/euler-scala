73ff9b2450cd57c1df3bb9df067f2120f2d51297
Michael Bayne <mdb@mandevilla.local>
2010-01-02 20:32:58 -0800

    Discovered the rhythm, thanks to paulp's clarification.

--- Euler89.scala
+++ Euler89.scala
@@ -4,7 +4,7 @@ object Euler89 extends EulerApp {
     case Nil => sum
     case h::t => fold(t, h, if (h >= last) sum+h else sum-h)
   }
-  def r2d (v :String) = fold(v.reverse.map(R2D).toList, 0, 0)
+  def r2d (v :String) = fold(v.reverse map R2D toList, 0, 0)
   val D2R = List("M"->1000, "CM"->900, "D"->500, "CD"->400, "C"->100, "XC"->90,
                  "L"->50, "XL"->40, "X"->10, "IX"->9, "V"->5, "IV"->4, "I"->1)
   def d2r (n :Int, ts :List[Pair[String,Int]] = D2R) :String = ts match {
