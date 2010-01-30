b3fd573f610c527965f36aae7ed0a80f4ab7659b
Michael Bayne <mdb@mandevilla.local>
2010-01-30 12:33:14 -0800

    Use our genprimes() since it's way faster.

--- Euler10.scala
+++ Euler10.scala
@@ -1,13 +1,3 @@
-object Euler10 extends Application {
-  var numbers = List.range(2,1000000).toArray;
-  def sumprimes (idx :Int, sum :Long) :Long = {
-    var prime = numbers(idx);
-    for (midx <- List.range(idx, numbers.length, prime)) {
-      numbers(midx) = 0;
-    }
-    val nidx = numbers.slice(idx, numbers.length).findIndexOf((a) => (a != 0));
-    if (nidx == -1) return sum + prime;
-    else return sumprimes(idx + nidx, sum + prime);
-  }
-  println(sumprimes(0, 0));
+object Euler10 extends EulerApp {
+  println(genprimes(2000000).foldLeft(0L)(_+_))
 }
