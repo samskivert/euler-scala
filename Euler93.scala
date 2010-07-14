087efe708a920fb6f67433a35c2cac78f6dd7d66
Michael Bayne <mdb@samskivert.com>
2010-07-13 22:33:23 -0700

    Somewhat verbose solution to 93.

---
+++ Euler93.scala
@@ -0,0 +1,34 @@
+object Euler93 extends EulerApp {
+  trait Expr { def eval :Float }
+  case class Plus (a :Expr, b :Expr) extends Expr { def eval = a.eval + b.eval }
+  case class Minus (a :Expr, b :Expr) extends Expr { def eval = a.eval - b.eval }
+  case class Mult (a :Expr, b :Expr) extends Expr { def eval = a.eval * b.eval }
+  case class Div (a :Expr, b :Expr) extends Expr { def eval = a.eval / b.eval }
+  case class Const (c :Float) extends Expr { def eval = c }
+  implicit def intToConst (c :Int) = Const(c)
+
+  type Gen = (Expr, Expr) => Expr
+  val opgen = List(Plus.apply _, Minus.apply _, Mult.apply _, Div.apply _)
+
+  def gentrees (vals :List[Int], gen :(Gen, Gen, Gen, Int, Int, Int, Int) => Expr) =
+    for (o1 <- opgen; o2 <- opgen; o3 <- opgen; l1 <- vals;
+         val vals2 = vals filterNot l1.==; l2 <- vals2;
+         val vals3 = vals2 filterNot l2.==; l3 <- vals3;
+         val vals4 = vals3 filterNot l3.==; l4 <- vals4)
+    yield gen(o1, o2, o3, l1, l2, l3, l4)
+
+  def reduce (es :Seq[Expr]) = es map(e => e.eval) filter(r => r > 0 && r.toInt == r) map(_.toInt)
+
+  def gendigs (vals :List[Int]) = {
+    val lefts = gentrees(vals, (o1, o2, o3, l1, l2, l3, l4) => o1(o2(o3(l1, l2), l3), l4))
+    val rights = gentrees(vals, (o1, o2, o3, l1, l2, l3, l4) => o1(l1, o2(l2, o3(l3, l4))))
+    val bals = gentrees(vals, (o1, o2, o3, l1, l2, l3, l4) => o1(o2(l1, l2), o3(l3, l4)))
+    scala.collection.immutable.TreeSet[Int]() ++ reduce(lefts) ++ reduce(rights) ++ reduce(bals)
+  }
+
+  val sets = for (a <- 1 to 10; b <- (a+1) to 10;
+                  c <- (b+1) to 10; d <- (c+1) to 10) yield List(a, b, c, d)
+
+  def proc (l :List[Int]) = (l.mkString, gendigs(l).foldLeft(0)((k, v) => if (k == v-1) v else k))
+  println(sets map(proc) reduceLeft((b, a) => if (a._2 > b._2) a else b) _1)
+}
