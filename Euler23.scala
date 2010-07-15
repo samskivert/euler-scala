9ceb730d3c28e93e8e931dba6a83cc5408c508e1
Michael Bayne <mdb@mandevilla.local>
2010-07-14 21:51:31 -0700

    Semicolon patrol!

--- Euler23.scala
+++ Euler23.scala
@@ -1,20 +1,20 @@
-import scala.collection.mutable.Set;
+import scala.collection.mutable.Set
 
 object Euler23 {
   def divisors (x :Int) = Set(1) ++
-    (2 to (math.sqrt(x).toInt) filter(x%_ == 0) flatMap(n => List(n, x/n)));
+    (2 to (math.sqrt(x).toInt) filter(x%_ == 0) flatMap(n => List(n, x/n)))
 
   def filter (abund :Array[Int], ints :Set[Int], a :Int, b :Int) :Set[Int] = {
-    ints -= (abund(a) + abund(b));
+    ints -= (abund(a) + abund(b))
     if (b == abund.length-1)
-      if (a == abund.length-1) ints;
-      else filter(abund, ints, a+1, 0);
-    else filter(abund, ints, a, b+1);
+      if (a == abund.length-1) ints
+      else filter(abund, ints, a+1, 0)
+    else filter(abund, ints, a, b+1)
   }
 
   def main (args :Array[String]) {
-    val MAX = 28123;
-    val abundant = 1 until MAX filter(a => (a < divisors(a).sum)) toArray;
-    println(filter(abundant, Set() ++ List.range(1, MAX+1), 0, 0).sum);
+    val MAX = 28123
+    val abundant = 1 until MAX filter(a => (a < divisors(a).sum)) toArray
+    println(filter(abundant, Set() ++ List.range(1, MAX+1), 0, 0).sum)
   }
 }
