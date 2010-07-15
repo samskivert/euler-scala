73dad95b8a0d5ac178924655aa7c72532176d8c6
Michael Bayne <mdb@mandevilla.local>
2010-07-14 21:48:13 -0700

    More compact and slightly more efficient divisors() method.

--- Euler23.scala
+++ Euler23.scala
@@ -1,13 +1,8 @@
 import scala.collection.mutable.Set;
 
 object Euler23 {
-  def divisors (x :Int) = 1 :: (for { 
-    divis <- List.range(2, math.sqrt(x).toInt+1)
-    if x % divis == 0
-  } yield List(divis, x / divis).distinct).flatten
-
-  val MAX = 28123;
-  val abundant = List.range(1, MAX+1).filter(a => (a < divisors(a).sum)).toArray;
+  def divisors (x :Int) = Set(1) ++
+    (2 to (math.sqrt(x).toInt) filter(x%_ == 0) flatMap(n => List(n, x/n)));
 
   def filter (abund :Array[Int], ints :Set[Int], a :Int, b :Int) :Set[Int] = {
     ints -= (abund(a) + abund(b));
@@ -16,7 +11,10 @@ object Euler23 {
       else filter(abund, ints, a+1, 0);
     else filter(abund, ints, a, b+1);
   }
+
   def main (args :Array[String]) {
+    val MAX = 28123;
+    val abundant = 1 until MAX filter(a => (a < divisors(a).sum)) toArray;
     println(filter(abundant, Set() ++ List.range(1, MAX+1), 0, 0).sum);
   }
 }
