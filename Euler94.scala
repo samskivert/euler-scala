ba1cc12bcb656e5db159037f40c0d5c608aa824c
Michael Bayne <mdb@mandevilla.local>
2010-07-16 05:04:14 -0700

    Some tweaks. This approach is totally off base.

--- Euler94.scala
+++ Euler94.scala
@@ -1,12 +1,13 @@
 object Euler94 {
   val MAX = 1000000000L/3
-  def check (a :Long, b :Long) = {
+  def check (a :Double, b :Double) :Long = {
     val area = b * math.sqrt(a*a - b*b)
-    if (area == area.toLong) 2*a+2*b else 0
+    if (area == area.toLong) (2*a+2*b).toLong
+    else 0
   }
   def scan (a :Long, accum :Long) :Long = {
     if (a > MAX) accum
-    else scan(a+2, accum + check(a, (a-1)/2) + check(a, (a+1)/2))
+    else scan(a+1, accum + check(a, (a-1)/2.0) + check(a, (a+1)/2.0))
   }
   def main (args :Array[String]) {
     println(scan(3, 0))
