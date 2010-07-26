4196d6b70e7e4a6cfde2e796d10bf6e49d73bb1b
Michael Bayne <mdb@mandevilla.local>
2010-07-26 09:06:28 -0700

    Solution to 99.

---
+++ Euler99.scala
@@ -0,0 +1,4 @@
+object Euler99 extends EulerApp {
+  val nums = readlines("base_exp.txt") map(_.split(",")) map(a => (a(0).toInt, a(1).toInt))
+  def answer = ((nums map(p => p._2 * math.log(p._1)) zipWithIndex) sortWith(_._1>_._1) head)._2 + 1
+}
