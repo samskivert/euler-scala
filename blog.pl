cad7bfb1d1fc0b220f9cb42c6d2b90a30eb6bcfb
Michael Bayne <mdb@samskivert.com>
2010-08-21 16:59:01 -0700

    Add a link to the source on Github.

--- blog.pl
+++ blog.pl
@@ -2,7 +2,9 @@
 
 my $number = shift or die "Usage: $0 N\n";
 my $url = "http://projecteuler.net/index.php";
-printf("<a href=\"$url?section=problems&id=%d\">Problem %03d</a>:\n", $number, $number);
+printf("<a href=\"$url?section=problems&id=%d\">Problem %03d</a> ", $number, $number);
+my $giturl = "http://github.com/samskivert/euler-scala/blob/master";
+printf("(<a href=\"$giturl/Euler%03d.scala\">source</a>):\n", $number);
 print("[scala]\n");
 open(CODE, "Euler$number.scala") or die "Can't read Euler$number.scala: $!\n";
 while (<CODE>) {
