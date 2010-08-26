d66bc2da6f41c204b3c197061828aa358aa3f908
Michael Bayne <mdb@baltic.(none)>
2010-08-26 10:01:36 -0700

    Cleanup prior to posting.

--- Euler064.scala
+++ Euler064.scala
@@ -1,17 +1,16 @@
 object Euler064 extends EulerApp {
   case class Root (root :Int, add :Int, div :Int) {
     def expand = {
-      val term = ((Math.sqrt(root.toDouble) + add)/div).toInt
-      val nadd = (term*div)-add
-      (term, Root(root, nadd, (root + -nadd*nadd)/div))
+      val term = ((math.sqrt(root) + add)/div).toInt
+      val nadd = term*div - add
+      (term, Root(root, nadd, (root - nadd*nadd)/div))
     }
     override def toString = "(√" + root + "+" + add + ")/" + div
   }
-
   def expansion (terms :List[Int], roots :List[Root]) :List[Int] = {
-    val (term, root) = roots.last.expand
-    if (root.div == 0 || roots.indexOf(root) != -1) terms + term
-    else expansion(terms + term, roots + root)
+    val (term, root) = roots.head.expand
+    if (root.div == 0 || roots.contains(root)) term :: terms
+    else expansion(term :: terms, root :: roots)
   }
-  println(1.to(10000).map(n => expansion(Nil, Nil+Root(n, 0, 1)).length).filter(_%2==0).length)
+  def answer = 1 to 10000 map(n => expansion(Nil, Root(n, 0, 1)::Nil).length) count(_%2==0)
 }
