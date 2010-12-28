38db035b0054b79b40138c2aeef7edd92a6c135a
Michael Bayne <mdb@samskivert.com>
2010-12-28 15:27:44 -0800

    Various optimizations which bring running time down from ~4 minutes to 1.2 seconds. Still not as fast as Prim's or Krustal's algorithm, but fast enough for our purposes.

--- Euler107.scala
+++ Euler107.scala
@@ -1,18 +1,17 @@
 object Euler107 extends EulerApp {
   def connected (matrix :List[Array[Int]]) = {
-    def loop (node :Array[Int], seen :Set[Int]) :Set[Int] = {
-      val reached = (node.zipWithIndex filter(_._1 != 0) map(_._2) toSet) -- seen
-      if (reached.isEmpty) seen
-      else reached flatMap(v => loop(matrix(v), seen ++ reached))
+    val cmatrix = matrix.map(r => r.zipWithIndex map(c => if (c._1 == 0) 0 else c._2) toSet)
+    def loop (node :Set[Int], seen :Set[Int]) :Set[Int] = {
+      val reached = node -- seen
+      ((seen ++ reached) /: reached)((nseen, v) => loop(cmatrix(v), nseen))
     }
-    loop(matrix.head, Set(0)).size == matrix.size
+    loop(cmatrix.head, Set(0)).size == matrix.size
   }
-  val network = readlines("network.txt") map(_ split(",") map(_.replace('-','0')) map(_.toInt))
-  val edges = network.zipWithIndex flatMap(
-    r => r._1.zipWithIndex filter(c => c._1 != 0 && c._2 > r._2) map(c => (c._1, r._2, c._2)))
   def answer = {
-    val osum = edges map(_._1) sum
-    var nsum = osum
+    val network = readlines("network.txt") map(_.replace('-','0') split(",") map(_.toInt))
+    val edges = network.zipWithIndex flatMap(
+      r => r._1.zipWithIndex filter(c => c._1 != 0 && c._2 > r._2) map(c => (c._1, r._2, c._2)))
+    var saved = 0
     for (e <- edges sortWith(_._1 > _._1)) {
       network(e._2)(e._3) = 0
       network(e._3)(e._2) = 0
@@ -20,9 +19,9 @@ object Euler107 extends EulerApp {
         network(e._2)(e._3) = e._1
         network(e._3)(e._2) = e._1
       } else {
-        nsum -= e._1
+        saved += e._1
       }
     }
-    osum - nsum
+    saved
   }
 }
