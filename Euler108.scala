1d5f82b1156ab187367a4b25991a60ce004fe32d
Michael Bayne <mdb@samskivert.com>
2011-01-12 17:32:59 -0800

    Solutions to Euler 108 and 110. 108 vexed me for much longer than any Euler problem I can recall. (Producing five or six pages filled with numbers in my notebooks). In the end, of course, I was going about it in a far more complicated manner than necessary, and the real solution turned out to be surprisingly simple.

--- Euler108.scala
+++ Euler108.scala
@@ -1,17 +1,9 @@
 object Euler108 extends EulerApp {
-  def facts (n :Int) = ((1 to n/2 filter(n%_ == 0)) :+ n) toList
-  def sols (n :Int) = {
-    def check (f :List[Int], acc :Set[Int]) :Set[Int] = f match {
-      case Nil => acc
-      case h :: t => check(t, acc ++ (t map(_ + h) filter(s => acc forall(s%_ != 0))))
-    }
-    val s = check(facts(n), Set())
-    if (s.size > 250) println(n + " -> " + s)
-    s
+  def find (base :Seq[Int], m :Int) :Int = {
+    val s = base ++ factors(m)
+    val sols = (s ++ s).groupBy(x => x).map(_._2.size).map(_+1).product/2+1
+    if (sols > 1000) base.product * m
+    else find(base, m+1)
   }
-  // def answer = Stream.from(1) find(n => sols(n).size > 999) get
-  // def answer = 100000 to 110000 count(n => sols(n).size > 999)
-  // def answer = 100000 to 110000 count(n => facts(n).size > 999)
-  // def answer = 1 to 50 foreach { n => println(n + " -> " + (n*(n+1)/2-(n-1))) }
-  def answer = 1 to 10 foreach { n => println(n + " -> " + sols(n)) }
+  def answer = find(primes.take(6), 2)
 }
---
+++ Euler110.scala
@@ -0,0 +1,9 @@
+object Euler110 extends EulerApp {
+  def find (base :Seq[Int], m :Int) :Long = {
+    val s = base ++ factors(m)
+    val sols = (s ++ s).groupBy(x => x).map(_._2.size).map(_+1).product/2+1
+    if (sols > 4000000) base.map(_.toLong).product * m
+    else find(base, m+1)
+  }
+  def answer = find(primes.take(12), 2)
+}
