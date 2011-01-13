ab1be91f12a978d301b3c2a8a83ee4783b5673a1
Michael Bayne <mdb@samskivert.com>
2011-01-12 17:26:00 -0800

    Added comments, and primes which returns a Stream of primes, and factors which returns all prime factors rather than just unique prime factors (and which uses the primes stream).

--- EulerApp.scala
+++ EulerApp.scala
@@ -2,6 +2,7 @@ import scala.io.Source
 import java.io.File
 
 abstract class EulerApp {
+  /** Checks the primality of `n`. Slowly. */
   def isprime (n :Int) :Boolean = {
     val limit = math.sqrt(n).toInt+1
     var ii = 2
@@ -11,6 +12,8 @@ abstract class EulerApp {
     }
     true
   }
+
+  /** Checks the primality of `n`. Slowly. */
   def isprime (n :Long) :Boolean = {
     val limit = math.sqrt(n).toInt+1
     var ii = 2
@@ -20,6 +23,8 @@ abstract class EulerApp {
     }
     true
   }
+
+  /** Checks the primality of `n`. Slowly. */
   def isprime (n :BigInt) :Boolean = {
     val limit = math.sqrt(n.doubleValue).toInt+1
     var ii = 2
@@ -30,6 +35,8 @@ abstract class EulerApp {
     true
   }
 
+  /** Generates an array containing primes < `range`.
+   *  Filter with `r.filter(0.!=)` to extract just the primes. */
   def genprimes (range :Int) :Array[Int] = {
     val primes = Array.range(0, range)
     primes(1) = 0
@@ -42,8 +49,18 @@ abstract class EulerApp {
     return primes
   }
 
+  /** Returns the greatest common divisor of `n` and `d`. */
   def gcd (n :Int, d :Int) :Int = if (d == 0) n else gcd(d, n%d)
 
+  /** Generates a stream of primes. */
+  def primes = {
+    def from (n: Int): Stream[Int] = Stream.cons(n, from(n + 1))
+    def sieve (s: Stream[Int]): Stream[Int] =
+      Stream.cons(s.head, sieve(s.tail filter { _ % s.head != 0 }))
+    sieve(from(2))
+  }
+
+  /** Returns unique prime factors of the supplied `n`. */
   def primefacts (primes :Iterable[Int], n :Int) = {
     var facts :List[Int] = Nil
     var curn = n
@@ -54,18 +71,37 @@ abstract class EulerApp {
     if (curn == 1) facts else curn :: facts
   }
 
+  /** Returns all prime factors of the supplied `n`. */
+  def factors (n :Int) = {
+    def loop (n :Int, primes :Stream[Int], facts :List[Int]) :List[Int] = {
+      val p = primes.head
+      if (n % p == 0) loop(n/p, primes, p :: facts)
+      else if (n > p) loop(n, primes.tail, facts)
+      else facts
+    }
+    loop(n, primes, Nil)
+  }
+
+  /** Reads the first line from the supplied source file. */
   def readline (file :String) :String =
     Source.fromFile(file).getLines().next.stripLineEnd
+
+  /** Reads the first line from the supplied source file and splits on `,`. */
   def readwords (file :String) :List[String] =
     readline(file).split(',').map(n => n.slice(1, n.length-1)).toList.sortWith(_<_)
+
+  /** Reads the first line from the supplied source file, splits on `,` and `toInt`s. */
   def readnums (file :String) :List[Int] =
     readline(file).split(',').toList.map(_.toInt)
+
+  /** Reads all lines from the supplied source file, stripping line endings. */
   def readlines (file :String) :List[String] =
     Source.fromFile("data" + File.separator + file).getLines().map(_.stripLineEnd).toList
 
   // computes and returns the solution
   def answer :Any
 
+  /** The main entry point for an Euler solution. Just calls `answer`. */
   def main (args :Array[String]) {
     println(answer)
   }
