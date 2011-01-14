0faafb8819aa9823884d8debfad7cbfcf8534b97
Michael Bayne <mdb@samskivert.com>
2011-01-13 16:01:07 -0800

    Solution to 109.

---
+++ Euler109.scala
@@ -0,0 +1,19 @@
+object Euler109 extends EulerApp {
+  val singles = (1 to 20) :+ 25
+  val doubles = singles map(_*2)
+  val triples = 1 to 20 map(_*3)
+  val zones = (singles ++ doubles ++ triples) sortBy(-_) toList
+
+  def tails[A] (l :List[A]) :List[List[A]] = if (l.isEmpty) Nil else l :: tails(l.tail)
+
+  def checkouts (score :Int) = {
+    def outs (points :Int, darts :Int, zs :List[Int]) :Int = {
+      if (points == 0) 1
+      else if (darts == 0) 0
+      else tails(zs dropWhile(_ > points)) map(t => outs(points-t.head, darts-1, t)) sum
+    }
+    doubles takeWhile(_ <= score) map(z => outs(score-z, 2, zones)) sum
+  }
+
+  def answer = 1 until 100 map(checkouts) sum
+}
