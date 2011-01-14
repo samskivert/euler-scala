8cb0ee682a67b379d4421d83066f28e1c519f69e
Michael Bayne <mdb@samskivert.com>
2011-01-13 16:09:49 -0800

    Tidying and comment.

--- Euler109.scala
+++ Euler109.scala
@@ -4,6 +4,7 @@ object Euler109 extends EulerApp {
   val triples = 1 to 20 map(_*3)
   val zones = (singles ++ doubles ++ triples) sortBy(-_) toList
 
+  // e.g. List(1, 2, 3) -> List(List(1, 2, 3), List(2, 3), List(3))
   def tails[A] (l :List[A]) :List[List[A]] = if (l.isEmpty) Nil else l :: tails(l.tail)
 
   def checkouts (score :Int) = {
@@ -12,7 +13,7 @@ object Euler109 extends EulerApp {
       else if (darts == 0) 0
       else tails(zs dropWhile(_ > points)) map(t => outs(points-t.head, darts-1, t)) sum
     }
-    doubles takeWhile(_ <= score) map(z => outs(score-z, 2, zones)) sum
+    doubles takeWhile(_ <= score) map(d => outs(score-d, 2, zones)) sum
   }
 
   def answer = 1 until 100 map(checkouts) sum
