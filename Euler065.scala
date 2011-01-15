46f943873b8135cb1c815bb7a2a74e95c7ddc97a
Michael Bayne <mdb@samskivert.com>
2011-01-14 18:30:46 -0800

    Tidied up for blogging.

--- Euler065.scala
+++ Euler065.scala
@@ -1,12 +1,12 @@
 object Euler065 extends EulerApp {
   case class Frac (numer :BigInt, denom :BigInt) {
-    def add (n :BigInt) = Frac(n * denom + numer, denom)
+    def + (n :BigInt) = Frac(n * denom + numer, denom)
     def invert = Frac(denom, numer)
   }
   def compute (count :Int, n :Int) :Frac = {
     val term = if (n == 1) 2 else if (n % 3 == 0) 2*(n/3) else 1
     if (n == count) Frac(term, 1)
-    else compute(count, n+1).invert.add(term)
+    else compute(count, n+1).invert + term
   }
-  println(compute(100, 1).numer.toString.toList.map(_-'0').sum)
+  def answer = compute(100, 1).numer.toString map(_-'0') sum
 }
