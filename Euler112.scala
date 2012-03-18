88896976fd664bbcdaef8599c0b77f0e0d25f9a6
Michael Bayne <mdb@samskivert.com>
2012-03-17 19:49:59 -0700

    HOF it up, for kicks.

--- Euler112.scala
+++ Euler112.scala
@@ -1,9 +1,7 @@
 object Euler112 extends EulerApp {
-  def isinc (n :Int, pd :Int = 9) :Boolean =
-    (n == 0) || (n%10 <= pd && isinc(n/10, n%10))
-  def isdec (n :Int, pd :Int = 0) :Boolean =
-    (n == 0) || (n%10 >= pd && isdec(n/10, n%10))
-  def isbouncy (n :Int) = !isinc(n+1) && !isdec(n+1)
+  def check (n :Int, pd :Int, comp :(Int, Int) => Boolean) :Boolean =
+    (n == 0) || (comp(n%10, pd) && check(n/10, n%10, comp))
+  def isbouncy (n :Int) = !check(n+1, 9, _ <= _) && !check(n+1, 0, _ >= _)
   def loop (n :Int, b :Int) :Int =
     if (99*n == 100*b) n
     else loop(n+1, if (isbouncy(n)) b+1 else b)
