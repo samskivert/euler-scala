f9d81b78bf9972e3c09c1ee31c479f64455359f7
Michael Bayne <mdb@samskivert.com>
2012-08-21 21:29:09 -0700

    Solution for 116.

---
+++ Euler116.scala
@@ -0,0 +1,9 @@
+object Euler116 extends EulerApp {
+  def count (len :Int) = new MemoMap[Int,Long] {
+    override def compute (remain :Int) =
+      if (remain < len) 0
+      else if (remain == len) 1
+      else 1 + apply(remain-len) + apply(remain-1)
+  }
+  def answer = count(4)(50) + count(3)(50) + count(2)(50)
+}
