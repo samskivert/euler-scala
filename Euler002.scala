1ae2b1893e4738179859bc4752231243e6f6dc65
Michael Bayne <mdb@samskivert.com>
2013-09-30 10:52:32 -0700

    Drive-by brevification.

--- Euler002.scala
+++ Euler002.scala
@@ -1,7 +1,5 @@
 object Euler002 extends EulerApp {
-  def fibby (a :Int, b :Int) :Int = {
-    val next = a + b;
-    return (if (b % 2 == 0) b else 0) + (if (next > 1000000) 0 else fibby(b, next));
-  }
-  def answer = fibby(1, 2)
+  def fib (a :Int, b :Int) :Int =
+    (if (b % 2 == 0) b else 0) + (if (a+b > 1000000) 0 else fib(b, a+b))
+  def answer = fib(1, 2)
 }
