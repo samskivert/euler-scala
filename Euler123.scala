07bdd901147bef0f2057c4d86903aac2a55b0d88
Michael Bayne <mdb@samskivert.com>
2014-08-01 11:10:31 -0700

    Inelegant solution for Euler123.

---
+++ Euler123.scala
@@ -0,0 +1,14 @@
+object Euler123 extends EulerApp {
+  def answer = {
+    val M = 10000000000L
+    val ps = genprimes(10000000)
+    def loop (n :Int) :Int = {
+      val p = ps(n-1) ; val p2 = p.toLong*p
+      def mpow (b :Long, exp :Long, acc :Long) :Long =
+        if (exp == 0) acc else mpow(b, exp-1, (acc * b) % p2)
+      if (p2 > M && (mpow(p-1, n, 1) + mpow(p+1, n, 1)) % p2 > M) n
+      else loop(n+1)
+    }
+    loop(7038)
+  }
+}
