06e6e06ba00103bd5db29906a9378480ccef24d0
Michael Bayne <mdb@samskivert.com>
2014-08-01 11:12:23 -0700

    Apply math: all even primes yield rem = 2.

--- Euler123.scala
+++ Euler123.scala
@@ -7,8 +7,8 @@ object Euler123 extends EulerApp {
       def mpow (b :Long, exp :Long, acc :Long) :Long =
         if (exp == 0) acc else mpow(b, exp-1, (acc * b) % p2)
       if (p2 > M && (mpow(p-1, n, 1) + mpow(p+1, n, 1)) % p2 > M) n
-      else loop(n+1)
+      else loop(n+2)
     }
-    loop(7038)
+    loop(7037)
   }
 }
