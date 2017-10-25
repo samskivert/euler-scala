34730f8f7b80c0d38686766a4c1f10fcd9588946
Michael Bayne <mdb@samskivert.com>
2017-10-25 15:35:29 -0700

    Fix typo.

--- Euler006.scala
+++ Euler006.scala
@@ -1,5 +1,5 @@
 object Euler006 extends EulerApp {
-  val first100sum = List.range(1,101).max
+  val first100sum = List.range(1,101).sum
   val first100sumsq = List.range(1,101).foldLeft(0)((b, a) => (b + a*a))
   def answer = first100sum * first100sum - first100sumsq
 }
